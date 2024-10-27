af12c83474c914f9be6cb03a3f1f8b8863f8798f
sanckh <corey.sutton7@gmail.com>
2024-10-26 21:55:47 -0500

    Fixing CORS errors, hopefully

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,11 +5,12 @@ import checkinRoutes from './routes/checkin_routes';
 import insightRoutes from './routes/insight_routes';
 import crisisRoutes from './routes/crisis_routes';
 import admin from 'firebase-admin';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 import userContactRoutes from './routes/userContacts_routes';
 import logRoutes from './routes/log_routes';
 import { requestLogger } from './utilities/logUtils';
 import rateLimit from 'express-rate-limit';
+import { Request, Response } from 'express';
 
 const app = express();
 const firebase = require('./firebase_options');
@@ -21,16 +22,35 @@ const logRateLimiter = rateLimit({
   message: 'Too many log requests. Please try again later.',
 });
 
+const allowedOrigins = [
+  'http://localhost:3000', 
+  '10.0.2.2:3000',
+  'https://mentalhealthapp-id5p.onrender.com',
+];
 
-const corsOptions = {
-  origin: true,
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
+
 
 app.use(cors(corsOptions));
 
-// Handle preflight requests for all routes
-app.options('*', cors(corsOptions));
+app.options('*', (req: Request, res: Response) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin || '');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(204); // No content for preflight
+});
 
 app.use(requestLogger);
 
