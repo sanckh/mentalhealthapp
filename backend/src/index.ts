0813fd94fa201968a6abcc7b852dbbef4d398bdd
sanckh <corey.sutton7@gmail.com>
2024-10-28 11:37:53 -0500

    Scaling back CORS for now

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,6 @@ import checkinRoutes from './routes/checkin_routes';
 import insightRoutes from './routes/insight_routes';
 import crisisRoutes from './routes/crisis_routes';
 import admin from 'firebase-admin';
-import * as cors from 'cors';
 import userContactRoutes from './routes/userContacts_routes';
 import logRoutes from './routes/log_routes';
 import { requestLogger } from './utilities/logUtils';
@@ -15,6 +14,7 @@ import { Request, Response } from 'express';
 const app = express();
 const firebase = require('./firebase_options');
 const port = process.env.PORT || 3000;
+var cors = require('cors')
 
 const logRateLimiter = rateLimit({
   windowMs: 60 * 1000,  // 1 minute window
@@ -22,35 +22,41 @@ const logRateLimiter = rateLimit({
   message: 'Too many log requests. Please try again later.',
 });
 
-const allowedOrigins = [
-  'http://localhost:3000', 
-  '10.0.2.2:3000',
-  'https://mentalhealthapp-id5p.onrender.com',
-];
 
-const corsOptions: cors.CorsOptions = {
-  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
+app.use(cors())
 
 
-app.use(cors.default(corsOptions));
+// const allowedOrigins = [
+//   'http://localhost:3000', 
+//   '10.0.2.2:3000',
+//   'https://mentalhealthapp-id5p.onrender.com',
+// ];
 
-app.options('*', (req: Request, res: Response) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin || '');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(204);
-});
+// const corsOptions: cors.CorsOptions = {
+//   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   },
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// };
+
+
+// app.use(cors.default(corsOptions));
+
+// app.options('*', (req: Request, res: Response) => {
+//   res.header('Access-Control-Allow-Origin', req.headers.origin || '');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.sendStatus(204);
+// });
+
+app.options('*', cors());
 
 app.use(requestLogger);
 
