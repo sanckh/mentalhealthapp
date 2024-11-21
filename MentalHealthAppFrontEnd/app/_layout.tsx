9ada8ce81218bcb1ce6a5350a5e62149a31791fa
missstaff <shawnastaff@gmail.com>
2024-11-20 19:19:43 -0800

    ready for pr

--- MentalHealthAppFrontEnd/app/_layout.tsx
+++ MentalHealthAppFrontEnd/app/_layout.tsx
@@ -2,16 +2,10 @@ import React from "react";
 import { Slot } from "expo-router";
 import { AuthContextProvider } from "./store/auth/auth-context";
 import { ThemeProvider } from "@/components/ThemeContext";
-import firebase from "@react-native-firebase/app";
 import useNotificationSetup from "@/hooks/useNotificationSetup";
 
-if (!firebase.apps.length) {
-  firebase.initializeApp({
-   // Add your firebase config here
-}
-
 export default function RootLayout() {
-  useNotificationSetup();
+  useNotificationSetup()
 
   return (
     <ThemeProvider>
