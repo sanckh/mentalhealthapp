0d3d0a745542b2227e16a27348056ad220e4dfd6
missstaff <shawnastaff@gmail.com>
2024-11-23 13:11:27 -0800

    google sign in with button needs client id, logic moved and wired into auth but we have a button and it works!

--- MentalHealthAppFrontEnd/app/authentication/LoginScreen.tsx
+++ MentalHealthAppFrontEnd/app/authentication/LoginScreen.tsx
@@ -17,6 +17,14 @@ import { useThemeContext } from "@/components/ThemeContext";
 import ResetPasswordModal from "@/components/modals/ResetPasswordModal";
 import { getData } from "../utilities/storage-utility";
 import { colors } from '../theme/colors';
+import {
+  GoogleSignin,
+  statusCodes,
+  isErrorWithCode,
+  isSuccessResponse,
+  isNoSavedCredentialFoundResponse,
+} from '@react-native-google-signin/google-signin';
+import { Platform } from 'react-native';
 
 export default function LoginScreen() {
   const { theme } = useThemeContext();
@@ -103,6 +111,45 @@ export default function LoginScreen() {
     }
   };
 
+  // #TODO MOVE GoogleSignin logic to a separate file
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: 'YOUR_GOOGLE_CLIENT_ID', // #TODO: Replace with client ID - get one from Google Developer Console
+      offlineAccess: true,
+    });
+  }, []);
+
+  const googleSignIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const response = await GoogleSignin.signIn();
+
+      if (isSuccessResponse(response)) {
+        // Read user's info
+        console.log(response);
+        return;
+      } 
+        await GoogleSignin.signIn();
+      
+    } catch (error) {
+      console.error("Google Sign-In error:", error);
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.SIGN_IN_CANCELLED:
+            console.log("User cancelled the login flow");
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            console.log("Play services not available or outdated");
+            break;
+          default:
+            console.log("Unknown error", error);
+        }
+      } else {
+        console.log("Non-Google Sign-In error occurred", error);
+      }
+    }
+  };
+
   return !isAuthenticated ? (
     <View style={styles.container}>
       {(isLoading || isAuthLoading) && (
@@ -167,6 +214,11 @@ export default function LoginScreen() {
           <Text style={styles.buttonText}>Login</Text>
         )}
       </Pressable>
+
+      <Pressable style={styles.button} onPress={googleSignIn}>
+        <Text style={styles.buttonText}>Sign in with Google</Text>
+      </Pressable>
+
       <Pressable
         style={styles.registerButton}
         onPress={() => router.replace("/register")}
